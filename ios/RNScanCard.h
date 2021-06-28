c3c2f52d21ff96fd1507b9197084e794a479294d
Thanushan <thanushan@calcey.com>
2021-06-28 13:16:36 +0530

    Updated imports

--- ios/RNScanCard.h
+++ ios/RNScanCard.h
@@ -14,11 +14,11 @@
 #import <React/RCTBridgeModule.h>
 #endif
 
-#if __has_include("RNBraintreeDropIn-Swift.h")
-#import "RNBraintreeDropIn-Swift.h"
-#else
-#import <RNBraintreeDropIn-Swift.h>
-#endif
+// #if __has_include("RNBraintreeDropIn-Swift.h")
+// #import "RNBraintreeDropIn-Swift.h"
+// #else
+// #import <RNBraintreeDropIn-Swift.h>
+// #endif
 
 @interface RNScanCard : NSObject <RCTBridgeModule>
 @end
