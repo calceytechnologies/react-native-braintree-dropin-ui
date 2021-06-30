1c48d396a0f1f441f611607c79cd8322275de361
Risina Perera <risina@calcey.com>
2021-06-30 11:56:55 +0530

    Update RNScanCard.m

--- ios/RNScanCard.m
+++ ios/RNScanCard.m
@@ -8,11 +8,11 @@ RCT_EXPORT_MODULE(RNScanCard)
 
 RCT_EXPORT_METHOD(show:(NSDictionary*)options resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-  dispatch_async(dispatch_get_main_queue(), ^(void) {
-      RNScanCardViewController *controller = [[RNScanCardViewController alloc]init];
-      UIViewController *rootViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
-      [rootViewController presentViewController:controller animated:YES completion:nil];
-  });
+//   dispatch_async(dispatch_get_main_queue(), ^(void) {
+//       RNScanCardViewController *controller = [[RNScanCardViewController alloc]init];
+//       UIViewController *rootViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
+//       [rootViewController presentViewController:controller animated:YES completion:nil];
+//   });
  
 }
 
